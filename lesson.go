8ddd419b83573b4211a287121b609131d5d43d9b
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-12-28 16:41:48 +0900

    8

--- lesson.go
+++ lesson.go
@@ -1,15 +1,31 @@
 package main
 
-// カッコを使うことで複数の指定をすることができるようになる
-// userを使うときは階層が一つ下なのでその部分の指定もしてあげる必要がある
 import (
 	"fmt"
-	"os/user"
-	"time"
 )
 
 func main() {
-	fmt.Println("Hello world", time.Now())
-	fmt.Println(user.Current())
-}
+	var i int = 1
+	var f64 = 1.2
+	var s string = "test"
+	var t bool = true
+	var f bool = false
+	fmt.Println(i,f64,s,t,f)
 
+	xi := 1 
+	// このような形で宣言することもできる
+	// ショートバージョンは関数内でしか使うことができない
+	// 型をしっかり定義する場合はvarを使うといい
+}
+// もし値を入れないとデフォルト値が表示される
+
+// func main() {
+// 	var (
+// 		 i int = 1
+// 		f64 = 1.2
+// 		s string = "test"
+// 		t bool = true
+// 		f bool = false
+// 	)
+// 		fmt.Println(i,f64,s,t,f)
+// } このような複数の宣言もすることができる
