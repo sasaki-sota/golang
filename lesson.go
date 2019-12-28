202b593e240852761e44914da87e4ac1f6dfd8a0
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-12-28 17:10:04 +0900

    10

--- lesson.go
+++ lesson.go
@@ -4,18 +4,16 @@ import (
 	"fmt"
 )
 
-// 書き換える必要のないものを定義するときに必要
-// cpnstはtypeを指定しない
-const Pi = 3.14
-
-var big int = 427312424343 + 1
-
-const (
-	username = "test_user"
-	Password = "test_pass"
-)
-
 func main() {
-	fmt.Println(Pi, username, Password)
-	fmt.Println(big)
+	var (
+		u8 uint8 		= 255
+		i8 int8 		= 127
+		f32 float32 	= 0.2
+		c64 complex64 	= -5 + 12i
+	)
+	// このように書くことが推奨されている
+	fmt.Println(u8, i8, f32, c64)
+
+	fmt.Printf("%T %v", u8, u8)
+	// タイプを調べるときにfを使う
 }
