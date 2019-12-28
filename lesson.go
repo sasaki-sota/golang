c51fa2d2a48f93a2e3301e6133c7e31ba464672e
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-12-28 16:27:12 +0900

    7

--- lesson.go
+++ lesson.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+// カッコを使うことで複数の指定をすることができるようになる
+// userを使うときは階層が一つ下なのでその部分の指定もしてあげる必要がある
+import (
+	"fmt"
+	"os/user"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Hello world", time.Now())
+	fmt.Println(user.Current())
 }
 
