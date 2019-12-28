34d80f33a7ec093bcd519b132d146161f4d776aa
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-12-28 20:58:23 +0900

    13

--- lesson.go
+++ lesson.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"strconv"
 	"fmt"
 )
-// boolearn型の指定の仕方
-func main() {
-	var t , f bool = true,false
-	fmt.Printf("%T %v\n", t, t)
-	fmt.Printf("%T %v\n", f, f)
 
-	fmt.Println(true && true)
-	fmt.Println(true && false)
-	fmt.Println(false && false)
-	// 比較をしてくれる
-	fmt.Println(true || true)
-	fmt.Println(true || false)
-	fmt.Println(false || false)
-	// こっちはどちらかがtrueであれば
+func main() {
+	var x int = 1
+	xx := float64(x)
+	fmt.Printf("%T %v %f\n", xx, xx, xx)
+	// このような形で肩の変換をすることができるようになる
+
+	var y float64 = 1.2
+	yy := int(y)
+	fmt.Printf("%T %v %d\n", yy,yy,yy)
+
+	var s string = "12"
+	i, _:= strconv.Atoi(s)
+	// strconvは文字列のコンバージョン
+	fmt.Printf("%T %v", i, i)
+	// _を使うことでエラーを防ぐことができる
 }
