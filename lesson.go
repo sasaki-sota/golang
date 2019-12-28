f23aa81d88d9d5fc25ac5286b3d1758eb2569604
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-12-28 23:47:43 +0900

    15

--- lesson.go
+++ lesson.go
@@ -5,14 +5,20 @@ import (
 )
 
 func main() {
-	var a [2]int
-	a[0]= 100
-	a[1] = 200
-	fmt.Println(a)
-
-	var b [2]int = [2]int{100, 200}
-	// これで初期値を設定することができる
-	fmt.Println(b)
-
-
+	n := []int{1,2,3,4,5,6}
+	fmt.Println(n)
+	fmt.Println(n[2])
+	fmt.Println(n[2:4])
+	fmt.Println(n[:2])
+	fmt.Println(n[2:])
+	fmt.Println(n[:])
+	// これがスライスの方法
+	// 指定した配列を出力することができるようになる
+	var board = [][]int{
+		[]int {0,1,2,},
+		[]int{3,4,5},
+		[]int{6,7,8},
+	}
+	fmt.Println(board)
+	// 複数の配列を表示することができるようになる
 }
