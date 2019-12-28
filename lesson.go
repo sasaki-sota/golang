009b896443276c4e102aad449ac3d47db3bf19e7
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-12-28 16:55:40 +0900

    9

--- lesson.go
+++ lesson.go
@@ -4,28 +4,18 @@ import (
 	"fmt"
 )
 
+// 書き換える必要のないものを定義するときに必要
+// cpnstはtypeを指定しない
+const Pi = 3.14
+
+var big int = 427312424343 + 1
+
+const (
+	username = "test_user"
+	Password = "test_pass"
+)
+
 func main() {
-	var i int = 1
-	var f64 = 1.2
-	var s string = "test"
-	var t bool = true
-	var f bool = false
-	fmt.Println(i,f64,s,t,f)
-
-	xi := 1 
-	// このような形で宣言することもできる
-	// ショートバージョンは関数内でしか使うことができない
-	// 型をしっかり定義する場合はvarを使うといい
+	fmt.Println(Pi, username, Password)
+	fmt.Println(big)
 }
-// もし値を入れないとデフォルト値が表示される
-
-// func main() {
-// 	var (
-// 		 i int = 1
-// 		f64 = 1.2
-// 		s string = "test"
-// 		t bool = true
-// 		f bool = false
-// 	)
-// 		fmt.Println(i,f64,s,t,f)
-// } このような複数の宣言もすることができる
