85cd68fbf81136079507fe65f6ec25d658c0b37b
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-12-29 07:50:25 +0900

    18

--- lesson.go
+++ lesson.go
@@ -4,14 +4,31 @@ import (
 	"fmt"
 )
 
-func main() {
-	b := []byte{72, 73}
-	// {}のカッコないで初期値を設定することができるようになる
-	fmt.Println(b)
-	fmt.Println(string(b))
-	// バイト型で表示するようになる
+func add(x int, y int) (int, int) {
+	return x + y, x - y
+}
+// 引数は第一引数と第二引数を用いる必要がある
 
-	c := []byte("HI")
-	fmt.Println(c)
-	
+// resultは変数名
+func cal(price, item int) (result int) {
+	result = price * item
+	return result
+}
+
+func converter(price int) float64 {
+	return float64(price)
+}
+
+func main() {
+	// add(10, 20)
+	r1, r2 := add(10, 20)
+	fmt.Println(r1, r2)
+
+	r3 := cal(100, 2)
+	fmt.Println(r3)
+
+	f := func() {
+		fmt.Println("inner func")
+	}
+	f(1)
 }
