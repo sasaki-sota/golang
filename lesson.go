2b3f18e33d7998dd004a1d905587f534da8b5ca3
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-12-29 11:15:17 +0900

    26

--- lesson.go
+++ lesson.go
@@ -5,17 +5,21 @@ import (
 )
 
 func main() {
-	// iは０で定義してセミコロンで続けて１０以下の場合はfor文で回すことができる
-	for i := 0; i<10; i++ {
-		if i ==3 {
-			fmt.Println("contineu")
-			continue
-		}
-		if i > 5{
-			fmt.Println("break")
-			break
-			// break文を入れると終了する
-		}
-		fmt.Println(i)
+	l := []string{"python", "go", "java"}
+
+	for i := 0; i < len(l); i++{
+		fmt.Println(i, l[i])
+	}
+
+	for i, v := range l{
+		fmt.Println(i, v)
+	}
+	// range文で取り出してくることができる
+	// _で取り出したい時のみに変更することができるようになる
+
+	m := map[string]int{"apple": 100, "banana": 200}
+
+	for v := range m {
+		fmt.Println(v)
 	}
 }
