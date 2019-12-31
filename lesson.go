fccf63ed9ef56c34f7d3b81b52b80224caa02637
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-12-31 10:29:34 +0900

    35

--- lesson.go
+++ lesson.go
@@ -4,21 +4,24 @@ import (
 	"fmt"
 )
 
-func one(x int) {
-	x = 1
-}
-
+// *を入れることによってバイトのポインタに関係することができるようになる
 func main() {
-	var n int = 100
-	one(n)
-	fmt.Println(n)
+	// var p *int = new(int)
+	// fmt.Println(*p)
+	// // アドレスは帰ってくる
+	// *p++
+	// fmt.Println(*p)
 
-	fmt.Println(&n)
+	// var p2 *int
+	// fmt.Println(p2)
 
-	var p *int = &n
+	// newとmakeの違い
+	s := make([]int, 0)
+	fmt.Printf("%T\n", s)
 
-	fmt.Println(p)
+	m := make(map[string]int)
+	fmt.Printf("%T\n", m)
 
-	fmt.Println(*p)
-	// アドレスの入るかた
+	var p *int = new(int)
+	fmt.Println("%T\n", p)
 }
