ee45fe0635e21b4e60732856455eefdafd136bbf
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-12-31 10:51:45 +0900

    39

--- lesson.go
+++ lesson.go
@@ -4,25 +4,29 @@ import (
 	"fmt"
 )
 
-// structはnilがない
 type Vertex struct {
-	// x int
-	// y int
 	X, Y int
 }
-// 大文字でないとできないので気をつける必要がある
+
+// classのように書くことができる
+func (v Vertex) Area() int{
+	return v.X * v.Y
+}
+
+func Area(v Vertex) int {
+	return v.X * v.Y
+}
+
+func (v *Vertex) Scale(i int){
+	v.X = v.X * i
+	v.Y = v.Y * i
+}
 
 func main() {
-	v := Vertex{x: 1, y: 2}
-	fmt.Println(v)
-	fmt.Println(v.x )
-	// 一つだけの取り組みも可能
+	v := Vertex{3, 4}
+	fmt.Println(Area(v))
+	fmt.Println(v.Area())
 
-	v2 := Vertex{x: 1}
-	fmt.Println(v2)
-
-	v3 := Vertex{}
-	fmt.Println(v3)
-
-	// 何も入れないとできない
+	v.Scale(10)
 }
+// .で表現できるものをメソッドという
