2a0254bce4068e4235470291c982c25c8f4dfedd
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-01-01 23:09:24 +0900

    perfect

--- web-application.go
+++ web-application.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"io/ioutil"
@@ -28,17 +28,37 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 // wに対しての返答が返ってくるような仕組みになっている
 func viewHandler(w http.ResponseWriter, r *http.Request){
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil{
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
 }
 
 // :の前に何も書かなければローカルホストになる
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 // ListenAndServeは第一引数はアドレスで第二引数はハンドラーとなっている
 // Fprintはioに関係するような出力の仕方
+
+
+// goでサーバを立ち上げるときに立ち上がらないときはgolangのディレクトリからpsとうち
+// kill -KILL 4757のような番号を入力して削除した再度でバックする必要がある
+// またはvsコードのていしボタンを押す
